b47484c22fb2c829182c5064cc725cb4bf2d6b74
Jishnu Sunil <jishnu.s@scalgo.net>
2023-07-04 10:03:47 +0530

    Designed base page for pushkart
    Designed base page for pushkart and set logo,nav bar, search bar etc..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { Dropdown} from 'bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pushkart';
+
+
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+  
+  toggle(modalElement: string | Element){
+    const modal=new Dropdown(modalElement);
+    modal.toggle();
+  }
+  
 }
